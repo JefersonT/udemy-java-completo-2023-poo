744f83aa1ddc33151524534c00aa604fd7041253
JefersonT <tqi_jtorres@uolinc.com>
2023-09-25 20:33:06 -0300

    Seção 14 - aula 157 Upcasting e Downcasting

---
+++ src/entities/SavingsAccount.java
@@ -0,0 +1,26 @@
+package entities;
+
+public class SavingsAccount extends Account {
+
+    private Double interestRate;
+
+    public SavingsAccount() {
+    }
+
+    public SavingsAccount(int number, String holder, double initialDeposit, Double interestRate) {
+        super(number, holder, initialDeposit);
+        this.interestRate = interestRate;
+    }
+
+    public void updateBalance() {
+        balance += balance * interestRate;
+    }
+
+    public Double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(Double interestRate) {
+        this.interestRate = interestRate;
+    }
+}
