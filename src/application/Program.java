6aa4aed0c6163827795ae89e8ead210e63abb4a4
JefersonT <tqi_jtorres@uolinc.com>
2023-09-26 21:09:03 -0300

    Seção 14 - aula 161 Exercício resolvido

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,41 +1,44 @@
 package application;
 
-import entities.Account;
-import entities.BusinessAccount;
-import entities.SavingsAccount;
+import entities.Employee;
+import entities.OutsourcedEmployee;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 public class Program {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+
+        System.out.printf("Enter the number of employees: ");
+        int qtd = sc.nextInt();
+        List<Employee> employees = new ArrayList<>();
+        for (int i = 1; i <= qtd; i++) {
+            System.out.println("Employee #" + i + " data:");
+            System.out.printf("Outsourced (y/n)? ");
+            char ch = sc.next().charAt(0);
+            System.out.printf("Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.printf("Hours: ");
+            Integer hours = sc.nextInt();
+            System.out.printf("Value per hour: ");
+            Double valuePerHour = sc.nextDouble();
+            if (ch == 'y') {
+                System.out.printf("Additional charge: ");
+                Double additionalCharge = sc.nextDouble();
+                employees.add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge));
+            } else {
+                employees.add(new Employee(name, hours, valuePerHour));
+            }
+        }
+
+        System.out.println();
+        System.out.println("PAYMENTS:");
+        employees.stream().forEach(employee -> System.out.println(employee));
+
         sc.close();
-
-        Account acc = new Account(1001, "Alex", 0.0);
-        BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.00);
-
-        // UPCASTING
-        Account acc1 = bacc;
-        Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.00);
-        Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
-
-        //DOWNCASTING
-        BusinessAccount acc4 = (BusinessAccount) acc2;
-        acc4.loan(100.0);
-
-//        BusinessAccount acc5 = (BusinessAccount) acc3;
-
-        if (acc3 instanceof BusinessAccount) {
-            BusinessAccount acc5 = (BusinessAccount) acc3;
-            acc5.loan(200.0);
-            System.out.println("Loan!");
-        }
-
-        if (acc3 instanceof SavingsAccount) {
-            SavingsAccount acc5 = (SavingsAccount) acc3;
-            acc5.updateBalance();
-            System.out.println("Update!");
-        }
     }
 }
