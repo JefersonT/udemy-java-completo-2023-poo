24d2f352c7ad112b9529c9d63e7b7dbbfd4b6785
JefersonT <tqi_jtorres@uolinc.com>
2023-10-09 21:48:27 -0300

    exercicio de fixação

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,21 +11,21 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Enter account data");
+        System.out.printf("Number: ");
+        Integer number = sc.nextInt();
+        System.out.printf("Holder: ");
+        String holder = sc.next();
+        sc.nextLine();
+        System.out.printf("Initial balance: ");
+        Double balance = sc.nextDouble();
+        System.out.printf("Withdraw limit: ");
+        Double withdrawLimit = sc.nextDouble();
+
+        Account account = new Account(number, holder, balance, withdrawLimit);
+
+        System.out.println();
         try {
-            System.out.println("Enter account data");
-            System.out.printf("Number: ");
-            Integer number = sc.nextInt();
-            System.out.printf("Holder: ");
-            String holder = sc.next();
-            sc.nextLine();
-            System.out.printf("Initial balance: ");
-            Double balance = sc.nextDouble();
-            System.out.printf("Withdraw limit: ");
-            Double withdrawLimit = sc.nextDouble();
-
-            Account account = new Account(number, holder, balance, withdrawLimit);
-
-            System.out.println();
             System.out.printf("Enter amount for withdraw: ");
             account.withdraw(sc.nextDouble());
             System.out.println("new balance: " + account.getBalance());
